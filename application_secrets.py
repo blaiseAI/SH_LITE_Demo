354a94bc21a90bfa27558707ea7ebd73d87b7f0b
Blaise Sebagabo <bsebagabo1@studentmail.nait.ca>
2023-05-17 11:55:52 -0600

    Remove application_secrets from the repository

--- application_secrets.py
+++
@@ -1,8 +0,0 @@
-secrets = {
-    "MQTT_BROKER_IP_ADDRESS": "127.0.0.1",
-    "MQTT_BROKER_PORT": 1883,
-    # "MQTT_BROKER_IP_ADDRESS_CLOUD": "0466eb1a2b724e78bd7d90d597a225fd.s2.eu.hivemq.cloud",
-    # "MQTT_BROKER_PORT_CLOUD": 8883,
-    # "USERNAME_CLOUD": "hivemq.webclient.1683737168672",
-    # "PASSWORD_CLOUD": "7>n1f.?oKb9FREpD,0Bc",
-}
